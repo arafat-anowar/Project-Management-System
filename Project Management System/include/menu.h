b0cb2b614c625712c06a4fdd38b0b8176e4df6f1
arafat-anowar <arafat-anowar@outlook.com>
2026-07-28 17:43:47 +0600

    Implement Project Management Dashboard

--- Project Management System/include/menu.h
+++ Project Management System/include/menu.h
@@ -4,6 +4,9 @@
 #include<stdio.h>
 // Local Header Files
 #include "user.h"
+#include "auth.h"
+#include "project.h"
+#include "task.h"
 // Macros
 #define get_input _getch()
 #define ESC 27
@@ -14,4 +17,7 @@ int project_management_dashboard();
 int task_management_dashboard();
 int project_update_dashboard();
 int task_update_dashboard();
+int report_dashboard();
+int search_dashboard();
+
 #endif
--- Project Management System/src/menu.c
+++ Project Management System/src/menu.c
@@ -38,15 +38,16 @@ int dashboard()
     printf("+-------------------------[ DASHBOARD ]--------------------------+\n");
     printf("|   [1]   -> Project Management                                  |\n");
     printf("|   [2]   -> Task Managent                                       |\n");
-    printf("|   [3]   -> Search                                              |\n");
-    printf("|  [ESC]  -> Back                                                |\n");
+    printf("|   [3]   -> Reports                                             |\n");
+    printf("|   [4]   -> Search                                              |\n");
+    printf("|  [ESC]  -> Logout                                              |\n");
     printf("+----------------------[PRESS YOUR CHOICE]-----------------------+\n");
     // Take Input from user and validate that
     char choice;
     do
     {
         choice = get_input;
-    } while (choice < '1' || choice > '3');
+    } while (choice < '1' || choice > '4');
     switch (choice)
     {
     case '1':
@@ -55,14 +56,70 @@ int dashboard()
     case '2':
         task_management_dashboard();
         break;
+    case '3':
+        report_dashboard();
+        break;
+    case '4':
+        search_dashboard();
+        break;
     case ESC:
-        main_menu();
+        logout();
         break;
     }
+    return 0;
 }
 
 int project_management_dashboard()
 {
+    clear_screen();
+    header_screen();
+    printf("+--------------------[ PROJECT MANAGEMENT ]----------------------+\n");
+    printf("|   [1]   -> Create Project                                      |\n");
+    printf("|   [2]   -> Update Project                                      |\n");
+    printf("|   [3]   -> Delete Project                                      |\n");
+    printf("|   [4]   -> View All Projects                                   |\n");
+    printf("|   [5]   -> Search Project                                      |\n");
+    printf("|   [6]   -> Change Project Status                               |\n");
+    printf("|   [7]   -> Change Project Priority                             |\n");
+    printf("|   [8]   -> Extend Project Deadline                             |\n");
+    printf("|  [ESC]  -> Back                                                |\n");
+    printf("+---------------------[PRESS YOUR CHOICE]------------------------+\n");
+    // Take Input from user and validate that
+    char choice;
+    do
+    {
+        choice = get_input;
+    } while (choice < '1' || &choice > '8');
+    switch (choice)
+    {
+    case '1':
+        create_project();
+        break;
+    case '2':
+        update_project();
+        break;
+    case '3':
+        delete_project();
+        break;
+    case '4':
+        view_projects();
+        break;
+    case '5':
+        search_project();
+        break;
+    case '6':
+        change_project_status();
+        break;
+    case '7':
+        change_project_priority();
+        break;
+    case '8':
+        extend_project_deadline();
+        break;
+    case ESC:
+        dashboard();
+        break;
+    }
     return 0;
 }
 
@@ -77,6 +134,14 @@ int project_update_dashboard()
 }
 
 int task_update_dashboard()
+{
+    return 0;
+}
+int report_dashboard()
+{
+    return 0;
+}
+int search_dashboard()
 {
     return 0;
 }
