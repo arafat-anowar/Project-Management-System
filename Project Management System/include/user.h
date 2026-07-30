919564476ce484e33e692f63848da567e0f56fdf
Md. Lathfan Ibna Eusuf <lathfanlipto.work@gmail.com>
2026-07-31 03:21:56 +0600

    Implement Creat User

--- Project Management System/include/user.h
+++ Project Management System/include/user.h
@@ -1,6 +1,22 @@
 #ifndef USER_H
 #define USER_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+struct user
+{
+    char id[20];
+    char name_1st[20];
+    char name_last[20];
+    char name[30];
+    char email[30];
+    char phn_num[11];
+    char user_name[30];
+    char user_pass[20];
+    char security_ques[10];
+    char user_role[20];
+};
 // Function Prototypes
 int create_user();
 #endif
--- Project Management System/src/user.c
+++ Project Management System/src/user.c
@@ -2,5 +2,58 @@
 
 int create_user()
 {
+    struct user details;
+    generate_user_id(details.id);
+
+    printf("Enter your First Name : ");
+    fgets(details.name_1st, sizeof(details.name_1st), stdin);
+    details.name_1st[strcspn(details.name_1st,"\n")]='\0';
+    
+    printf("Enter your Last Name : ");
+    fgets(details.name_last, sizeof(details.name_last), stdin);
+    details.name_last[strcspn(details.name_last,"\n")]='\0';
+
+    printf("Enter your Full Name : ");
+    fgets(details.name, sizeof(details.name), stdin);
+    details.name[strcspn(details.name,"\n")]='\0';
+    
+    printf("Enter your Email@ : ");
+    fgets(details.email, sizeof(details.email), stdin);
+    details.email[strcspn(details.email,"\n")]='\0';
+
+    printf("Enter your Phone Number : ");
+    fgets(details.phn_num, sizeof(details.phn_num), stdin);
+    details.phn_num[strcspn(details.phn_num,"\n")]='\0';
+
+    printf("Enter your User : ");
+    fgets(details.user_name, sizeof(details.user_name), stdin);
+    details.user_name[strcspn(details.user_name,"\n")]='\0';
+
+    printf("Enter your Password : ");
+    fgets(details.user_pass, sizeof(details.user_pass), stdin);
+    details.user_pass[strcspn(details.user_pass,"\n")]='\0';
+
+    printf("Enter your favourite food : ");
+    fgets(details.security_ques, sizeof(details.security_ques), stdin);
+    details.security_ques[strcspn(details.security_ques,"\n")]='\0';
+
+    strcpy(details.user_role,"Individual");
+
+    FILE *file_open_for_write_data;
+    file_open_for_write_data = fopen("database\\userDBS.csv", "a");
+    fprintf(file_open_for_write_data,
+            "%d,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
+            details.id,
+            details.name_1st,
+            details.name_last,
+            details.name,
+            details.email,
+            details.phn_num,
+            details.user_name,
+            details.user_pass,
+            details.security_ques,
+            details.user_role);            
+    fclose(file_open_for_write_data);
+
     return 0;
 }
