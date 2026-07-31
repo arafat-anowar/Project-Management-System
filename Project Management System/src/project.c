f2cd941e359b8bdfbe5894ce43eb2dcb01cff243
Md. Lathfan Ibna Eusuf <lathfanlipto.work@gmail.com>
2026-07-31 21:40:03 +0600

    Fix User Create Issue

--- Project Management System/src/project.c
+++ Project Management System/src/project.c
@@ -1,17 +1,67 @@
 #include "project.h"
 
+struct project data;
 int create_project()
 {
+
+    printf("\nProject name : ");
+    fgets(data.name, sizeof(data.name), stdin);
+    data.name[strcps(data.name, "\n")] = "\0";
+
+    printf("\nCategory : ");
+    fgets(data.category, sizeof(data.category), stdin);
+    data.category[strcps(data.category, "\n")] = "\0";
+
+    printf("\nDescription : ");
+    fgets(data.description, sizeof(data.description), stdin);
+    data.description[strcps(data.description, "\n")] = "\0";
+
+    printf("\nPriority : ");
+    fgets(data.priority, sizeof(data.priority), stdin);
+    data.priority[strcps(data.priority, "\n")] = "\0";
+
+    printf("\nStatus : ");
+    fgets(data.name, sizeof(data.name), stdin);
+    data.name[strcps(data.name, "\n")] = "\0";
+
+    printf("\nProgress : ");
+    fgets(data.progress, sizeof(data.progress), stdin);
+    data.progress[strcps(data.progress, "\n")] = "\0";
+
+    printf("\nStart Date : ");
+    fgets(data.start_date, sizeof(data.start_date), stdin);
+    data.start_date[strcps(data.start_date, "\n")] = "\0";
+
+    printf("\nEnd data : ");
+    fgets(data.end_date, sizeof(data.end_date), stdin);
+    data.end_date[strcps(data.end_date, "\n")] = "\0";
+
+    printf("\nCreated by : ");
+    fgets(data.created_by, sizeof(data.created_by), stdin);
+    data.created_by[strcps(data.created_by, "\n")] = "\0";
+
     return 0;
 }
 
 int view_projects()
 {
+    char id[20], name[30];
+    if(search_by_project_id(*id, *name)==0)
+    {
+        printf("\t\t  ")
+
+    }    
+
+
+
+
+
     return 0;
 }
 
 int update_project()
 {
+
     return 0;
 }
 
--- Project Management System/src/user.c
+++ Project Management System/src/user.c
@@ -7,31 +7,36 @@ int create_user()
     strcpy(user.id, generate_user_id(user.id));
     printf("\nEnter Your Name : ");
     fgets(user.name, sizeof(user.name), stdin);
-    user.name[strcps(user.name, "\n")] = "\0";
+    user.name[strcpn(user.name, "\n")] = "\0";
 
     printf("\nEnter Your Email : ");
     fgets(user.email, sizeof(user.email), stdin);
-    user.email[strcps(user.email, "\n")] = "\0";
+    user.email[strcpn(user.email, "\n")] = "\0";
 
     printf("\nEnter Your Phone Number : ");
     fgets(user.phone, sizeof(user.phone), stdin);
-    user.phone[strcps(user.phone, "\n")] = "\0";
+    user.phone[strcpn(user.phone, "\n")] = "\0";
 
     printf("\nEnter Your User Name : ");
     fgets(user.user_name, sizeof(user.user_name), stdin);
-    user.user_name[strcps(user.user_name, "\n")] = "\0";
+    user.user_name[strcpn(user.user_name, "\n")] = "\0";
 
     printf("\nEnter Your Password : ");
     fgets(user.pass, sizeof(user.pass), stdin);
-    user.pass[strcps(user.pass, "\n")] = "\0";
+    user.pass[strcpn(user.pass, "\n")] = "\0";
 
     printf("\nWhat's Your Favourite Food : ");
     fgets(user.security_ques, sizeof(user.security_ques), stdin);
-    user.security_ques[strcps(user.security_ques, "\n")] = "\0";
+    user.security_ques[strcpn(user.security_ques, "\n")] = "\0";
 
     printf("\nWhat Is Your Role on the Project");
     fgets(user.role, sizeof(user.role), stdin);
-    user.role[strcps(user.role, "\n")] = "\0";
+    user.role[strcpn(user.role, "\n")] = "\0";
+
+    FILE *file_open_for_write_data;
+    file_open_for_write_data = fopen("database\\userDBS.csv", "a");
+    fprintf(file_open_for_write_data, "ID: %20d, Name: %30s, Email: %30s, Phone Number: %11s, User Name: %20s, Password: %30s, Favourite Food: %15s, Role: %20s\n", user.id, user.name, user.email, user.phone, user.user_name, user.pass, user.security_ques, user.role);
+    fclose(file_open_for_write_data);
 
     return 0;
 }
