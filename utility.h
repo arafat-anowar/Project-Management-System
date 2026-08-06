d686872d3541cb05690d0029911f31fc14747b35
faizalabiba20-spec <faizalabiba20@gmail.com>
2026-08-06 23:24:24 +0600

    Create utility.h

---
+++ utility.h
@@ -0,0 +1,11 @@
+int  header_screen()
+{
+    clear_screen();
+    printf("******************************************************************\n");
+    printf("*                                                                *\n");
+    printf("*                  PROJECT MANAGEMENT SYSTEM                     *\n");
+    printf("*                                                                *\n");
+    printf("******************************************************************\n");
+    printf("\n");
+return 0;
+}
