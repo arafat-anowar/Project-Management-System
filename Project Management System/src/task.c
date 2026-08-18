ddb26be52d8f8b61de48bdb890a64bd7ad79774c
arafat-anowar <arafat-anowar@outlook.com>
2026-08-18 22:46:07 +0600

    add comments in view tasks

--- Project Management System/src/task.c
+++ Project Management System/src/task.c
@@ -1062,29 +1062,41 @@ int delete_task()
 
 int view_tasks()
 {
+    // declare all variables
     struct t_details task;
     int terminal_width = ZERO, terminal_height = ZERO, box_width = CONTAINER_WIDTH, box_height = TASK_DETAILS_BOX_HEIGHT, x = ZERO, y = ZERO;
     char path[TASK_PATH_BUFFER_SIZE], row[TASK_FILE_DATA_SIZE], *field;
     FILE *taskDBS_open;
 
+    // set terminal for UTF8 and show header screen
     init_console();
     header_screen();
 
+    // measure terminal height and width also x and y coordinate
     terminal_width = get_console_width();
     terminal_height = get_console_height();
-
     x = (terminal_width - box_width) / TWO;
     y = ((terminal_height - box_height) / TWO) + SCREEN_START_Y;
 
+    // get task database path
     get_path(path);
     strcat(path, TASK_DATABASE_FILE);
 
+    // open database
     taskDBS_open = fopen(path, FILE_MODE_READ);
 
+    if (taskDBS_open == NULL)
+    {
+        something_went_wrong_screen(FILE_OPEN_ERROR);
+        return 0;
+    }
+
+    // read database
     while (fgets(row, sizeof(row), taskDBS_open) != NULL)
     {
         row[strcspn(row, "\n")] = '\0';
 
+        // tokenize data
         field = strtok(row, ",");
         task.unique_id = atoi(field);
 
@@ -1120,6 +1132,7 @@ int view_tasks()
 
         task_details_screen(x, y);
 
+        // print task details
         move_cursor(x + PROJECT_INPUT_X, y + TASK_DETAILS_Y_UNIQUE_ID_OFFSET);
         printf("%d ", task.unique_id);
 
@@ -1150,7 +1163,11 @@ int view_tasks()
         get_input;
     }
 
-    fclose(taskDBS_open);
+    // close database
+    if (fclose(taskDBS_open) == EOF)
+    {
+        something_went_wrong_screen(FILE_CLOSE_ERROR);
+    }
 
     return 0;
 }
